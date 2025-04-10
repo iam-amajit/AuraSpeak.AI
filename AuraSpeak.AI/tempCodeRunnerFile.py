62bd40a0549ed01a18f734ea39c1495305c8202b
Amajit Mukherjee <amajitmukherjee11@gmail.com>
2025-04-10 20:58:30 +0530

    Add files via upload

---
+++ AuraSpeak.AI/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+@app.route('/home')
+# def home():
+#     return redirect('/')
+# @app.route('/')
+# def submit():
+#     while True:
+#         run_alexa()
+#         return render_template("alexa.html") 
